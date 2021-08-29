6abab2daa524e37180f39602ca23658097f38329
dingchao <1109928059@qq.com>
2021-08-29 23:24:20 +0800

    load data success

--- recommender/dataloader/src/main/scala/com/atguigu/dataloader/DataLoader.scala
+++ recommender/dataloader/src/main/scala/com/atguigu/dataloader/DataLoader.scala
@@ -1,5 +1,10 @@
 package com.atguigu.dataloader
 
+import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.casbah.{MongoClient, MongoClientURI}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
 /**
  * Movies数据集, 数据集字段通过 ^分割
  *
@@ -40,36 +45,153 @@ case class Rating(val uid: Int, val mid: Int, val score: Double, val timestamp: 
 
 case class Tag(val uid: Int, val mid: Int, val tag: String, val timestamp: Int)
 
+/**
+ * MongoDB connection configuration
+ *
+ * @param uri MongoDB的连接
+ * @param db  MongoDB要操作的数据库
+ */
+case class MongoConfig(val uri: String, val db: String)
+
+/**
+ * elasticsearch connection configuration
+ *
+ * @param httpHosts      Http的主机列表, 以,分割
+ * @param transportHosts Transport主机列表, 以,分割
+ * @param index          需要操作的索引
+ * @param clusterName    ES集群的名称
+ */
+case class ESConfig(val httpHosts: String, val transportHosts: String, val index: String, val clusterName: String)
+
 // 数据的主加载服务
 object DataLoader {
+
+    val MOVIE_DATA_PATH = "D:\\IDEA_Projects\\MovieRecommendSystem\\recommender\\dataloader\\src\\main\\resources\\movies.csv"
+    val RATING_DATA_PATH = "D:\\IDEA_Projects\\MovieRecommendSystem\\recommender\\dataloader\\src\\main\\resources\\ratings.csv"
+    val TAG_DATA_PATH = "D:\\IDEA_Projects\\MovieRecommendSystem\\recommender\\dataloader\\src\\main\\resources\\tags.csv"
+
+    val MONGODB_MOVIE_COLLECTION = "Movie"
+    val MONGODB_RATING_COLLECTION = "Rating"
+    val MONGODB_TAG_COLLECTION = "Tag"
+
+    val ES_MOVIE_INDEX = "Movie"
 
     // 程序的入口
     def main(args: Array[String]): Unit = {
 
+        val config = Map(
+            "spark.cores" -> "local[*]",
+            "mongo.uri" -> "mongodb://192.168.177.128:27017/recommender",
+            "mongo.db" -> "recommender",
+            "es.httpHosts" -> "192.168.177.128:9200",
+            "es.transportHosts" -> "192.168.177.128:9300",
+            "es.index" -> "recommender",
+            "es.cluster.name" -> "elasticsearch"
+        )
+
         // 需要创建SparkConfig配置
-        val sparkConf = null;
+        val sparkConf = new SparkConf().setAppName("DataLoader").setMaster(config.get("spark.cores").get)
 
         // 创建一个SparkSession
-        val spark = null;
+        val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+
+        import spark.implicits._
 
         // 将movies, ratings, tags数据集加载进来
-        val movieRDD = null;
-        val ratingRDD = null;
-        val tagRDD = null;
+        val movieRDD = spark.sparkContext.textFile(MOVIE_DATA_PATH)
+        val ratingRDD = spark.sparkContext.textFile(RATING_DATA_PATH)
+        val tagRDD = spark.sparkContext.textFile(TAG_DATA_PATH)
+
+        val movieDF = movieRDD.map(item => {
+            val attr = item.split("\\^")
+            Movie(
+                attr(0).toInt,
+                attr(1).trim,
+                attr(2).trim,
+                attr(3).trim,
+                attr(4).trim,
+                attr(5).trim,
+                attr(6).trim,
+                attr(7).trim,
+                attr(8).trim,
+                attr(9).trim
+            )
+        }).toDF()
+
+        val ratingDF = ratingRDD.map(item => {
+            val attr = item.split(",")
+            Rating(
+                attr(0).toInt,
+                attr(1).toInt,
+                attr(2).toDouble,
+                attr(3).toInt
+            )
+        }).toDF()
+
+        val tagDF = tagRDD.map(item => {
+            val attr = item.split(",")
+            Tag(
+                attr(0).toInt,
+                attr(1).toInt,
+                attr(2).trim,
+                attr(3).toInt
+            )
+        }).toDF()
+
+        implicit val mongoConfig = MongoConfig(config.get("mongo.uri").get, config.get("mongo.db").get)
 
         // 需要将数据保存到MongoDB中
-        storeDataInMongoDB()
+        storeDataInMongoDB(movieDF, ratingDF, tagDF)
 
         // 需要将数据保存到es中
-        storeDataInES()
+        // storeDataInES()
 
         // 关闭Spark
-        // spark.stop
+        spark.stop()
     }
 
     // 将数据加载到MongoDB中的方法
-    def storeDataInMongoDB(): Unit = {
+    def storeDataInMongoDB(movieDF: DataFrame, ratingDF: DataFrame, tagDF: DataFrame)(implicit mongoConfig: MongoConfig): Unit = {
 
+        // 新建一个到MongoDB的连接
+        val mongoClient = MongoClient(MongoClientURI(mongoConfig.uri))
+
+        // 如果MongoDB中有对应的数据库, 应该删除
+        mongoClient(mongoConfig.db)(MONGODB_MOVIE_COLLECTION).dropCollection()
+        mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).dropCollection()
+        mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION).dropCollection()
+
+        // 将当前数据写入到MongoDB
+        movieDF.write
+            .option("uri", mongoConfig.uri)
+            .option("collection", MONGODB_MOVIE_COLLECTION)
+            .mode("overwrite")
+            .format("com.mongodb.spark.sql")
+            .save()
+
+        ratingDF.write
+            .option("uri", mongoConfig.uri)
+            .option("collection", MONGODB_RATING_COLLECTION)
+            .mode("overwrite")
+            .format("com.mongodb.spark.sql")
+            .save()
+
+        tagDF.write
+            .option("uri", mongoConfig.uri)
+            .option("collection", MONGODB_TAG_COLLECTION)
+            .mode("overwrite")
+            .format("com.mongodb.spark.sql")
+            .save()
+
+        // 对数据表建索引
+        mongoClient(mongoConfig.db)(MONGODB_MOVIE_COLLECTION).createIndex(MongoDBObject("mid" -> 1))
+        mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).createIndex(MongoDBObject("mid" -> 1))
+        mongoClient(mongoConfig.db)(MONGODB_RATING_COLLECTION).createIndex(MongoDBObject("uid" -> 1))
+        mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION).createIndex(MongoDBObject("mid" -> 1))
+        mongoClient(mongoConfig.db)(MONGODB_TAG_COLLECTION).createIndex(MongoDBObject("uid" -> 1))
+
+        // 关闭MongoDB连接
+        mongoClient.close()
     }
 
     // 将数据保存到ES中的方法
